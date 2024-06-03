149d93b952072eab98dc9ce207f2482cd5fa893b
codyngpriest <vilhopriestly@gmail.com>
2024-06-03 06:34:03 +0000

    implement 1-binary_tree_insert_left.c

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Tasks
 
 0. New node
 This is a function that creates a binary tree node
+
+1. Insert left
+This is a function that inserts a node as the left-child of another node
---
+++ binary_trees.h
@@ -0,0 +1,76 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+typedef struct binary_tree_s binary_tree_t;
+
+/* Binary Search Tree */
+typedef struct binary_tree_s bst_t;
+/* AVL Tree */
+typedef struct binary_tree_s avl_t;
+
+void binary_tree_print(const binary_tree_t *);
+
+/* Function prototypes */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
+size_t _binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
+bst_t *bst_remove(bst_t *root, int value);
+bst_t *bst_find_min(bst_t *tree);
+int binary_tree_is_avl(const binary_tree_t *tree);
+avl_t *avl_insert(avl_t **tree, int value);
+avl_t *avl_balance(avl_t *node);
+avl_t *avl_insert_recursive(avl_t *root, int value);
+avl_t *array_to_avl(int *array, size_t size);
+avl_t *balance_subtree(avl_t *node);
+avl_t *rebalance(avl_t *node, avl_t **tree);
+bst_t *replace(bst_t *node, bst_t *new, int far_left);
+avl_t *balance_left(avl_t *node);
+
+#endif /* BINARY_TREES_H */
